c2226c83951c3c9fed18488f1182c74f269b0a5d
Anilton Cardoso <acardosoj@gmail.com>
2024-07-09 21:40:40 -0300

    ASDASDASDAD

---
+++ src/funcoes.py
@@ -0,0 +1,16 @@
+def remover_coluna_missing(df, col_name, limite = 0.10):
+    print("OIIIIIIIIIIIIIIIIIIIII")
+    print("OLÁAAAAAAAAAAAAAAAAAA")
+    if df[col_name].isna().mean() > limite:
+        df = df.drop(columns = col_name)
+    return df
+
+def analisar_outlier(df, col):
+    q1 = df[col].quantile(0.25)
+    q3 = df[col].quantile(0.75)
+    iqr = q3 - q1
+    li = q1 - 1.5 * iqr
+    ls = q3 + 1.5 * iqr
+    df = df[df[col] >= li]
+    df = df[df[col] <= ls]
+    return df 
